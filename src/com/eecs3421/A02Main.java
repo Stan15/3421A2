ed04998272b4d365776295d4d447150bb9971ca0
Hashir <hashir.jamil17@gmail.com>
2022-03-19 22:11:19 -0400

    Added course information and id# for grading

--- src/com/eecs3421/A02Main.java
+++ src/com/eecs3421/A02Main.java
@@ -1,5 +1,10 @@
 package com.eecs3421;
 
+/*
+	Hashir Jamil - 217 452 854
+	Stanley Ihesiulo - 216 985 236
+	EECS 3421 Winter 2022 Assignment 02
+ */
 public class A02Main {
 
 	//The purpose of this class is to start the program and display the GUI
