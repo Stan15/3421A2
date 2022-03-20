dce32209a16d6a86276d360ff9237489fa716280
Hashir <hashir.jamil17@gmail.com>
2022-03-19 23:30:17 -0400

    Added connection parameters in a02

--- src/com/eecs3421/A02MiddleTier.java
+++ src/com/eecs3421/A02MiddleTier.java
@@ -1,5 +1,13 @@
 package com.eecs3421;
 
+/*
+ Connection Parameters:
+ IP: 127.0.0.1,
+ Port: 3306,
+ User: root,
+ Password: root1234
+ */
+
 public class A02MiddleTier {
 	//This class will contain your code for interacting with Database, acquire the query result and display it in the GUI text area.
 
