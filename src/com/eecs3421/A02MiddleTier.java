0d325c2b35bc53326b981c8b5bcd917ad9b47730
Hashir <hashir.jamil17@gmail.com>
2022-03-26 17:17:22 -0400

    Created all non date specified queries and started logic for button detection

--- src/com/eecs3421/A02MiddleTier.java
+++ src/com/eecs3421/A02MiddleTier.java
@@ -8,7 +8,107 @@ package com.eecs3421;
  Password: root1234
  */
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class A02MiddleTier {
 	//This class will contain your code for interacting with Database, acquire the query result and display it in the GUI text area.
+
+    String userName = "root";
+    String password = "root1234";
+    String serverName = "127.0.0.1";
+    String portNumber = "3306";
+
+    //query library
+
+    String q_conf_all = "SELECT E.Name, C.City, C.Country, C.EvDate, E.EventWebLink, E.CFPText " +
+                        "FROM Event as E, EventConference as C " +
+                        "WHERE E.ID = C.EventID;";
+
+    String q_conf_period = "";
+
+    String q_journal_all = "SELECT E.Name, J.JournalName, J.Publisher, E.EventWebLink, E.CFPText " +
+                           "FROM Event as E, EventJournal as J " +
+                           "WHERE E.ID = J.EventID;";
+
+    String q_journal_period = "";
+
+    String q_book_all = "SELECT E.Name, B.Publisher, E.EventWebLink, E.CFPText " +
+                        "FROM Event as E, EventBook as B " +
+                        "WHERE E.ID = B.EventID;";
+
+    String q_book_period = "";
+
+    String q_conf_journal_all = "SELECT Name, EventWebLink, CFPText " +
+                                "FROM Event as E RIGHT JOIN EventConference as C ON E.ID = C.EventID " +
+                                "UNION " +
+                                "SELECT Name, EventWeblink, CFPText " +
+                                "FROM Event as E RIGHT JOIN EventJournal as J on E.ID = J.EventID;";
+
+    String q_conf_journal_period = "";
+
+    String q_conf_book_all = "SELECT Name, EventWebLink, CFPText " +
+                             "FROM Event AS E RIGHT JOIN EventConference AS C ON E.ID = C.EventID " +
+                             "UNION SELECT Name, EventWeblink, CFPText " +
+                             "FROM Event AS E RIGHT JOIN EventBook AS B on E.ID = B.EventID;";
+
+    String q_conf_book_period = "";
+
+    String q_book_journal_all = "SELECT Name, EventWebLink, CFPText " +
+                                "FROM Event as E RIGHT JOIN EventJournal as J ON E.ID = J.EventID " +
+                                "UNION " +
+                                "SELECT Name, EventWeblink, CFPText " +
+                                "FROM Event as E RIGHT JOIN EventBook as B on E.ID = B.EventID; ";
+
+    String q_book_journal_period = "";
+
+    String q_event_all = "SELECT Name, EventWebLink, CFPText " +
+                         "From Event;";
+
+    String q_event_period = "";
+
+
+    /*
+    Connection to MySQL database
+    Source code from https://docs.oracle.com/javase/tutorial/jdbc/basics/connecting.html
+     */
+    public Connection getConnection() throws SQLException {
+
+        Connection conn = null;
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", this.userName);
+        connectionProps.put("password", this.password);
+
+        conn = DriverManager.getConnection(
+                "jdbc:" + "mysql" + "://" +
+                        this.serverName +
+                        ":" + this.portNumber + "/",
+                connectionProps);
+
+        System.out.println("Connected to database");
+        return conn;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    private String selectionCheck(JCheckBox conf, JCheckBox journ, JCheckBox book, JRadioButton all , JRadioButton date, JTextField from, JTextField to) {
+
+        String outputQuery;
+
+        //all three boxes checked and no period specified
+        if (true) {
+            outputQuery = q_event_all;
+            return outputQuery;
+        }
+
+        return outputQuery;
+
+    }
 
 }
